d2c3cae05355e206091082022c9353abfbabc795
LimarenkoDenis <limarenkodenis@gmail.com>
2018-11-15 11:18:45 +0200

    add or update cart

--- src/app/reducers/cart.reducer.ts
+++ src/app/reducers/cart.reducer.ts
@@ -1,22 +1,26 @@
-import { ICart } from './../interfaces/product.interface';
+import { ICart, IProduct } from './../interfaces/product.interface';
 import { CartActionTypes, CartActions } from './../actions/cart.actions';
 import { Action } from '@ngrx/store';
 
 
-export const initialState: ICart[]  = [];
+export const initialState: ICart[] = [];
 
 export function reducer(state = initialState, action: CartActions): ICart[] {
   switch (action.type) {
     case CartActionTypes.AddToCart: {
-      const index: number = state.findIndex(({id}) => id === action.payload.id);
+      const newState: ICart[] = state.slice();
+      const product: IProduct = action.payload;
+      const index: number = newState.findIndex((item: ICart) => item.id === product.id);
 
-      if ( index === -1) {
-        return [...state, {...action.payload, amount: 1}];
-
+      if (index === -1) {
+        return [...newState, { ...product, amount: 1 }];
       }
 
-      return state;
+      const element: ICart = newState[index];
+      newState.splice(index, 1, { ...element, amount: element.amount + 1 });
+      return newState;
     }
+
     default:
       return state;
   }
